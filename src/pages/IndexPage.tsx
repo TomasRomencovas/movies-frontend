48e06e68ea56c9a36911e2fb9401e1e2b3836a62
Tomas Romencovas <tomas.romencovas@gmail.com>
2025-04-24 22:39:02 +0300

    testing hosted backend

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -9,15 +9,15 @@ export type MovieType = {
   rating: number[];
   imgurl: string;
   description: string;
-}
+};
 
 export default function IndexPage() {
   const [movies, setMovies] = useState<MovieType[]>([]);
-  const [searchInput, setSearchInput] = useState("")
-  const [filteredMovies, setFilteredMovies] = useState<MovieType[]>([])
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState<MovieType[]>([]);
   const [limit, setLimit] = useState<number | null>(2);
-  const [offset, setOffset] = useState(0)
-  const [noMoviesFound, setNoMoviesFound] = useState("")
+  const [offset, setOffset] = useState(0);
+  const [noMoviesFound, setNoMoviesFound] = useState("");
 
   const navigate = useNavigate();
 
@@ -25,8 +25,9 @@ export default function IndexPage() {
 
   useEffect(() => {
     axios
-      .get("http://localhost:3000/validateToken", { 
-        withCredentials: true, })
+      .get("https://movies-backend-4bx3.onrender.com/validateToken", {
+        withCredentials: true,
+      })
       .then(() => {
         setIsAuthenticated(true);
       })
@@ -37,45 +38,54 @@ export default function IndexPage() {
 
   useEffect(() => {
     axios
-      .get("http://127.0.0.1:3000/movies", {params: { limit: limit, offset: offset }})
+      .get("https://movies-backend-4bx3.onrender.com/movies", {
+        params: { limit: limit, offset: offset },
+      })
       .then((response) => setMovies(response.data));
   }, [limit, offset]);
 
-
   useEffect(() => {
     if (searchInput) {
-      setLimit(null)
-      setOffset(0)
-      if(!limit) {
-        const newFilteredMovies = movies.filter((movie) => movie.name.toLowerCase().includes(searchInput.toLowerCase()))
-        setFilteredMovies(newFilteredMovies)
-        if(!newFilteredMovies[0]) {
-          setNoMoviesFound("Movie not found.")
+      setLimit(null);
+      setOffset(0);
+      if (!limit) {
+        const newFilteredMovies = movies.filter((movie) =>
+          movie.name.toLowerCase().includes(searchInput.toLowerCase())
+        );
+        setFilteredMovies(newFilteredMovies);
+        if (!newFilteredMovies[0]) {
+          setNoMoviesFound("Movie not found.");
         } else {
-          setNoMoviesFound("")
+          setNoMoviesFound("");
         }
       }
     } else {
-      setNoMoviesFound("")
-      setFilteredMovies([])
-      setLimit(2)
+      setNoMoviesFound("");
+      setFilteredMovies([]);
+      setLimit(2);
     }
-  }, [searchInput, limit, movies])
+  }, [searchInput, limit, movies]);
 
-function onLogout() {
-  axios
-  .post("http://localhost:3000/logout", {}, { withCredentials: true })
-  .then(() => setIsAuthenticated(false));
+  function onLogout() {
+    axios
+      .post(
+        "https://movies-backend-4bx3.onrender.com/logout",
+        {},
+        { withCredentials: true }
+      )
+      .then(() => setIsAuthenticated(false));
   }
-
-  
-  
 
   return (
     <div>
       <div className="search">
         <h1>Movies</h1>
-        <input type="text" placeholder="Search by movie name" value={searchInput} onChange={(e) => setSearchInput(e.target.value)}/>
+        <input
+          type="text"
+          placeholder="Search by movie name"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
         <br />
         {isAuthenticated ? (
           <div>
@@ -92,24 +102,37 @@ function onLogout() {
         <hr />
       </div>
       <div className="movies">
-        {!noMoviesFound && (filteredMovies[0] && filteredMovies || movies).map((movie) => (
-          <div onClick={() => navigate(`/movie/${movie.id}`)} key={movie.id}>
-            <img src={movie.imgurl} alt="" />
-            <h3>{movie.name}</h3>
-            <h4>
-              {movie.rating
-                ? "Score: " +
-                  (movie.rating.reduce((acc, cur) => acc + cur, 0) /
-                    movie.rating.length).toFixed(1) +
-                  "/5"
-                : "Not Rated"}
-            </h4>
-          </div>
-        ))}
+        {!noMoviesFound &&
+          ((filteredMovies[0] && filteredMovies) || movies).map((movie) => (
+            <div onClick={() => navigate(`/movie/${movie.id}`)} key={movie.id}>
+              <img src={movie.imgurl} alt="" />
+              <h3>{movie.name}</h3>
+              <h4>
+                {movie.rating
+                  ? "Score: " +
+                    (
+                      movie.rating.reduce((acc, cur) => acc + cur, 0) /
+                      movie.rating.length
+                    ).toFixed(1) +
+                    "/5"
+                  : "Not Rated"}
+              </h4>
+            </div>
+          ))}
         {noMoviesFound && <h3>{noMoviesFound}</h3>}
       </div>
-      <button hidden={limit === null || offset === 0} onClick={() => setOffset((prev) => prev - 2)}>Previous Page</button>
-      <button hidden={limit === null} onClick={() => setOffset((prev) => prev + 2)}>Next Page</button>
+      <button
+        hidden={limit === null || offset === 0}
+        onClick={() => setOffset((prev) => prev - 2)}
+      >
+        Previous Page
+      </button>
+      <button
+        hidden={limit === null}
+        onClick={() => setOffset((prev) => prev + 2)}
+      >
+        Next Page
+      </button>
     </div>
   );
 }
